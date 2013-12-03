8d84abe32ea2b5ddc387456dc329a8b0beb5a93a
Andy Gu <andy@andy-VirtualBox.(none)>
2013-12-02 23:38:43 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Personalweb::Application.config.secret_key_base = 'aa304c5c7aa39e8ccdd4aaaabd5572e8dcd2afe97560f809e72cf9825ffc6d29a4dd41f58b07b1c658c0c85d7fab6ddb05ee3252ba0fb41bec5b7abb13a979b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Personalweb::Application.config.session_store :cookie_store, key: '_personalweb_session'
